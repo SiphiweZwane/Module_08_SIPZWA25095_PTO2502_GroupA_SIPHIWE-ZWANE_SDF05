a943d381b7a61a82a359f7e2e0e21aeaeb67a9fc
SiphiweZwane <zwanekamabena@gmail.com>
2025-04-17 14:47:55 +0200

    Added config files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Module_08_SIPZWA25095_PTO2502_GroupA_SIPHIWE-ZWANE_SDF05/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
